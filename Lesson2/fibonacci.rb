9b00a774f7776ae674ff9795bdf26f6ca1b3a421
Roma <abuhanifabaliev@gmail.com>
2023-12-22 18:04:06 +0300

    последние два. с покупками я почти сам сделал, а вот с днями у тебя списал

--- Lesson2/fibonacci.rb
+++ Lesson2/fibonacci.rb
@@ -1,2 +1,10 @@
-arr = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-puts arr
+#arr = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#puts arr
+
+array = [0, 1]
+
+while 100 > result = (array[-1] + array[-2])
+  array << result
+end
+
+puts array
