5449e7cf530d03d7c2ab6ea8c1d5b986576a73b9
Roma <abuhanifabaliev@gmail.com>
2023-12-25 12:06:28 +0300

    ...

--- Lesson2/fibonacci.rb
+++ Lesson2/fibonacci.rb
@@ -1,2 +1,9 @@
-arr = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-puts arr
+array = [0, 1]
+
+while 100 > result = (array[-1] + array[-2])
+  array << result
+end
+
+puts array
+
+#(array[-1] + array[-2]) - вот эту часть я не понял, а вот код вроде бы ясен.
